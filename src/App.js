9e67f0bc3b8388d57eb31a15d6aa8c49de5967fb
jSohnData <jihos@me.com>
2023-05-18 17:10:03 -0400

    FIXED css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.scss';
+
 
 function App() {
   return (
